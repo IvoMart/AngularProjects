fe787b37e14b31d0bddc11578e90a9362ae343bb
Ivan Martinez <ivan.martinez@dirmod.com>
2021-10-04 11:15:59 -0300

    add service HeroesApi and use it on Heroes comp

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,19 @@ import { Injectable } from '@angular/core';
 })
 export class HeroesService {
 
-  constructor() { }
-}
+  heroes: any[] = [];
+  respuesta: any[] = [];
+
+  constructor(private http: HttpClient) { 
+    this.cargarHeroes();
+  }
+
+  cargarHeroes() {
+    return this.http
+    .get('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json')
+    .subscribe( (respuesta : any) => {
+        this.heroes = respuesta;
+        console.log(this.heroes)
+      });
+    }
+  }
