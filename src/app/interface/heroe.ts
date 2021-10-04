865a37251781a293f623df121eaf1da59262cde8
Ivan Martinez <ivan.martinez@dirmod.com>
2021-10-04 10:40:18 -0300

    Change interface heroe

--- src/app/interface/heroe.ts
+++ src/app/interface/heroe.ts
@@ -1,2 +1,41 @@
 export interface Heroe {
+    id: string,
+    name: string,
+    slug: string,
+    powerstats: {
+        intelligence: string,
+        strength: string,
+        speed: string,
+        durability: string,
+        power: string,
+        combat: string
+    },
+    appearance: {
+        gender: string,
+        race: string,
+        eyeColor: string,
+        hairColor:string
+      },
+      biography: {
+        fullName: string,
+        alterEgos: string,
+        placeOfBirth: string,
+        firstAppearance: string,
+        publisher: string,
+        alignment: string
+      },
+      work: {
+        occupation: string,
+        base: string
+      },
+      connections: {
+        groupAffiliation: string,
+        relatives: string
+      },
+      images: {
+        xs: string,
+        sm: string,
+        md: string,
+        lg: string
+      }
 }
