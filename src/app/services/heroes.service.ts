6c7b543248145f5abeec89c73a362a4183f54776
Ivan Martinez <ivan.martinez@dirmod.com>
2021-10-04 11:19:11 -0300

    Modify list heroes: Heroes typpe

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Heroe } from '../interface/heroe';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
-  heroes: any[] = [];
+  heroes: Heroe[] = [];
   respuesta: any[] = [];
 
   constructor(private http: HttpClient) { 
@@ -16,7 +17,7 @@ export class HeroesService {
   cargarHeroes() {
     return this.http
     .get('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json')
-    .subscribe( (respuesta : any) => {
+    .subscribe( (respuesta: any) => {
         this.heroes = respuesta;
         console.log(this.heroes)
       });
