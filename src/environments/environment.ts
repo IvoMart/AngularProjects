c69cacb65c4a7802dc1a1ec429fb9afe2dd68191
Ivan Martinez <ivan.martinez@dirmod.com>
2021-10-20 09:24:04 -0300

    Last changes to Build production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  // production: false
+  production: true
 };
 
 /*
@@ -14,3 +15,10 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+/**
+ * 
+ * Comando of production build
+ * ng build --prod
+ * 
+ */
